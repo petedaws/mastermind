331c8fdfeeb85b97a7aa65a51d44e8881bf55a17
Peter Dawson <peter.a.dawson@boeing.com>
2021-07-31 19:43:14 +1000

    the clue results are now working

--- mastermind.py
+++ mastermind.py
@@ -7,6 +7,7 @@ DEFAULT_CODE_LENGTH=5
 
 WHITE={'name':'white','rbg':(255,255,255),'ansi':'\033[38;2;255;255;255m\u2588\033[0m'}
 RED={'name':'red','rbg':(255,0,0),'ansi':'\033[38;2;255;0;0m\u2588\033[0m'}
+BLACK={'name':'black','rbg':(0,0,0),'ansi':'\033[38;2;0;0;0m\u2588\033[0m'}
 
 BLUE={'name':'blue','rbg':(0,0,255),'ansi':'\033[38;2;0;0;255m\u2588\033[0m'}
 LTGRN={'name':'ltgrn','rbg':(22,221,53),'ansi':'\033[38;2;22;221;53m\u2588\033[0m'}   #
@@ -34,7 +35,36 @@ class bcolors:
 
 class GameBoard():
     pass
+
+def get_clue_str(code,secret):
+    clue = []
+    copy_code = code.code.copy()
+    copy_secret = secret.code.copy()
+    red_index = []
+    #identify the reds first and keep track these elements so we can remove them from the copy
+    for i,element in enumerate(copy_code):
+        if element == copy_secret[i]:
+            clue.append(RED)
+            red_index.append(element)
+            
+    #remove the indexes of items that match from the copy
+    for element in red_index:
+        copy_secret.remove(element)
+        copy_code.remove(element)
     
+    #now identify the whites
+    for i,element in enumerate(copy_code):
+        if element in copy_secret:
+            clue.append(WHITE)
+
+    #fill the remaining clue places with black
+    while len(clue) < len(secret.code):
+        clue.append(BLACK)
+    random.shuffle(clue)
+    o = Code(clue)
+    return o
+        
+   
 class GameState():
     def __init__(self):
         self.secret = Code()
@@ -49,9 +79,9 @@ class GameState():
         output = []
         
         for i,conjecture in enumerate(reversed(self.conjectures)):
-            output.append(f'#{len(self.conjectures)-i} {conjecture.str_ansi()}')
-        output.append(f'SECRET: {self.secret.str_ansi()}')
-        return '\n'.join(output)
+            output.append(f'#:{len(self.conjectures)-i}\t{conjecture.str_ansi()}\t[{get_clue_str(conjecture,self.secret).str_ansi()}]')
+        output.append(f'#:S\t{self.secret.str_ansi()}')
+        return '\n\n'.join(output)
         
 class Code():
     def __init__(self,code=[],code_length=DEFAULT_CODE_LENGTH):
@@ -69,12 +99,19 @@ class Code():
     def str_ansi(self):
         output = ' '.join([a['ansi'] for a in self.code])
         return output
+        
+    def str_hidden(self):
+        output = ' '.join(['X' for a in self.code])
+        return output
 
 def main():
     os.system("") # need to run the systemcommand to make ascii colours work
     gs = GameState()
     gs.make_conjecture([BLUE,LTGRN,GREEN,ORANGE,PURPLE])
-    gs.make_conjecture([YELLOW,GREY,PINK,WHITE,RED])
+    gs.make_conjecture([BLUE,BLUE,BLUE,BLUE,BLUE])
+    gs.make_conjecture([GREY,GREY,GREY,GREY,GREY])
+    gs.make_conjecture([LTGRN,LTGRN,LTGRN,LTGRN,LTGRN])
+    gs.make_conjecture([YELLOW,GREY,PINK,BLUE,LTGRN])
     print(gs)
 
 if __name__ == '__main__':
